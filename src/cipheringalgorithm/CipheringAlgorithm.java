4943059bae2259b60497fd89d980f51bd8674fa3
glvcsmt <>
2023-08-15 22:42:24 +0200

    Finished the first part of the project with the deciphering function. The next planned step is to write the tests.

--- src/cipheringalgorithm/CipheringAlgorithm.java
+++ src/cipheringalgorithm/CipheringAlgorithm.java
@@ -14,7 +14,6 @@ public class CipheringAlgorithm {
 
     //A karaktereket és kójaikat tartalmazó ArrayList feltöltése
     public static void Fill() {
-
         char c;
         int value = 0;
 
@@ -28,7 +27,6 @@ public class CipheringAlgorithm {
 
         Value space = new Value(' ', 26);
         values.add(space);
-
     }
 
     //Titkosítás
@@ -37,17 +35,13 @@ public class CipheringAlgorithm {
         Fill();
 
         char[] messageToChars = message.toCharArray();
-
         clue = clue.substring(0, messageToChars.length);
-
         char[] clueToChars = clue.toCharArray();
-
         String secret = "";
 
         for (int i = 0; i < messageToChars.length; i++) {
             int sum = 0;
-            char mess = messageToChars[i];
-            char cl = clueToChars[i];
+            char mess = messageToChars[i], cl = clueToChars[i];
 
             for (int j = 0; j < values.size(); j++) {
                 if (values.get(j).getC() == mess) {
@@ -71,11 +65,48 @@ public class CipheringAlgorithm {
         }
 
         System.out.println(secret);
+    }
 
+    //Megfejtés
+    public static void Deciphering(String secret, String clue) {
+
+        Fill();
+
+        char[] secretToChars = secret.toCharArray();
+        clue = clue.substring(0, secretToChars.length);
+        char[] clueToChars = clue.toCharArray();
+        String message = "";
+
+        for (int i = 0; i < secretToChars.length; i++) {
+            int secInt = 0, clInt = 0;
+            char sec = secretToChars[i], cl = clueToChars[i];
+
+            for (int j = 0; j < values.size(); j++) {
+                if (values.get(j).getC() == secretToChars[i]) {
+                    secInt = values.get(j).getValue();
+                }
+                if (values.get(j).getC() == clueToChars[i]) {
+                    clInt = values.get(j).getValue();
+                }
+            }
+
+            if (clInt > secInt) {
+                secInt += 27;
+            }
+
+            for (int j = 0; j < values.size(); j++) {
+                if (values.get(j).getValue() == (secInt - clInt)) {
+                    message += values.get(j).getC();
+                    break;
+                }
+            }
+        }
+        
+        System.out.println(message);
     }
 
     public static void main(String[] args) {
-
+        
     }
 
 }
